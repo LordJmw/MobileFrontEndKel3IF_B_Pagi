32ec3c2629209e5b41ac355cef70715712811fd5
LordJmw <jameswong5818@gmail.com>
2025-04-05 11:02:40 +0700

    Added lib/feat register page

---
+++ lib/feat/register.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  bool _obscureText = true;
+  bool _obscureTextConfirm = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Buat Akun Baru",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: "Poppins",
+                  color: Colors.black,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 290,
+                height: 40,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Nama Pengguna',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 0.3, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.blue, width: 2.5),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 290,
+                height: 40,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 0.5, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.blue, width: 2.5),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 290,
+                height: 40,
+                child: TextFormField(
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    labelText: 'Kata Sandi',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 0.5, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.blue, width: 2.5),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                      icon: Icon(
+                        _obscureText ? Icons.visibility_off : Icons.visibility,
+                      ),
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 290,
+                height: 40,
+                child: TextFormField(
+                  obscureText: _obscureTextConfirm,
+                  decoration: InputDecoration(
+                    labelText: 'Konfirmasi Kata Sandi',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 0.5, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _obscureTextConfirm = !_obscureTextConfirm;
+                        });
+                      },
+                      icon: Icon(
+                        _obscureTextConfirm
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                      color: Colors.grey,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.blue, width: 2.5),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                height: 50,
+                width: 290,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    backgroundColor: Colors.lightBlue,
+                    elevation: 10,
+                    shadowColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: const Text(
+                    'DAFTAR',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey,
+                      thickness: 0.5,
+                      indent: 70,
+                      endIndent: 10,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      "atau",
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey,
+                      thickness: 0.5,
+                      indent: 10,
+                      endIndent: 70,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                height: 50,
+                width: 290,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    backgroundColor: Colors.white,
+                    elevation: 10,
+                    shadowColor: const Color.fromARGB(255, 187, 186, 186),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/image/google.png',
+                        height: 20,
+                        width: 20,
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        'Daftar dengan Google',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Color.fromRGBO(68, 92, 136, 1),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 290,
+                child: Text.rich(
+                  TextSpan(
+                    text: "Dengan masuk ke Engliboo, anda menyetujui ",
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 119, 119, 119),
+                      fontFamily: "Poppins",
+                      fontSize: 12,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: "Ketentuan ",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(text: "dan "),
+                      TextSpan(
+                        text: "Kebijakan Privasi",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(text: " kami"),
+                    ],
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
